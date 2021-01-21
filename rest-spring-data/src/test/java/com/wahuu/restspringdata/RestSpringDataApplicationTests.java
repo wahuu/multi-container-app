2d9fa10f8a659c92d533a3d25565611af1d77b33
Mariusz Chrusciel <wahuu666@gmail.com>
2021-01-21 23:23:10 +0100

    Create template app for spring web, spring data

---
+++ rest-spring-data/src/test/java/com/wahuu/restspringdata/RestSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wahuu.restspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
